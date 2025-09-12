f3ff4e9b7911254292f7d8d0ec57397f74cc3f99
Samuel Hernandez <samueljhernandez99@gmail.com>
2025-09-12 08:13:14 -0700

    basic login

---
+++ Assets/Scripts/FirebaseController.cs
@@ -0,0 +1,266 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Extensions;
+
+public class FirebaseController : MonoBehaviour
+{
+    public GameObject loginPanel, signupPanel, profilePanel, resetPasswordPanel, notificationPanel;
+    public TMP_InputField loginEmail, loginPassword, signupEmail, signupPassword, signupCPassword, signupUserName, resetPassEmail;
+    public TMP_Text notif_Title_Text, notif_Message_Text, profileUserName_Text, profileUserEmail_Text;
+    public Toggle rememberMe;
+    Firebase.Auth.FirebaseAuth auth;
+    Firebase.Auth.FirebaseUser user;
+
+    bool isSignIn = false;
+
+    private void Start()
+    {
+        // Hide notification panel at the very beginning
+        if (notificationPanel != null)
+        {
+            notificationPanel.SetActive(false);
+        }
+
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                InitializeFirebase();
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+
+        OpenLoginPanel();
+    }
+
+    public void OpenLoginPanel()
+    {
+        loginPanel.SetActive(true);
+        signupPanel.SetActive(false);
+        profilePanel.SetActive(false);
+        resetPasswordPanel.SetActive(false);
+    }
+    public void OpenSignUpPanel()
+    {
+        loginPanel.SetActive(false);
+        signupPanel.SetActive(true);
+        profilePanel.SetActive(false);
+        resetPasswordPanel.SetActive(false);
+    }
+    public void OpenProfilePanel()
+    {
+        loginPanel.SetActive(false);
+        signupPanel.SetActive(false);
+        profilePanel.SetActive(true);
+        resetPasswordPanel.SetActive(false);
+    }
+
+    public void OpenResetPassPanel()
+    {
+        loginPanel.SetActive(false);
+        signupPanel.SetActive(false);
+        profilePanel.SetActive(false);
+        resetPasswordPanel.SetActive(true);
+    }
+
+    public void LoginUser()
+    {
+        if (string.IsNullOrEmpty(loginEmail.text) || string.IsNullOrEmpty(loginPassword.text))
+        {
+            showNotificationMessage("Error", "One or more Fields Empty");
+            return;
+        }
+
+        // Do login
+        SignInUser(loginEmail.text, loginPassword.text);
+    }
+
+    public void SignUpUser()
+    {
+        if (string.IsNullOrEmpty(signupEmail.text) || string.IsNullOrEmpty(signupPassword.text) || string.IsNullOrEmpty(signupCPassword.text) || string.IsNullOrEmpty(signupUserName.text))
+        {
+            showNotificationMessage("Error", "One or more Fields Empty");
+            return;
+        }
+
+        // Do Signup
+        CreateUser(signupEmail.text, signupPassword.text, signupUserName.text);
+    }
+
+    public void ResetPassword()
+    {
+        if (string.IsNullOrEmpty(resetPassEmail.text))
+        {
+            showNotificationMessage("Error", "Email Empty");
+            return;
+        }
+    }
+
+    private void showNotificationMessage(string title, string message)
+    {
+        notif_Title_Text.text = "" + title;
+        notif_Message_Text.text = "" + message;
+
+        notificationPanel.SetActive(true);
+    }
+
+    public void CloseNotif_Panel()
+    {
+        notif_Title_Text.text = "";
+        notif_Message_Text.text = "";
+
+        notificationPanel.SetActive(false);
+    }
+
+    public void LogOut()
+    {
+        auth.SignOut();
+        profileUserEmail_Text.text = "";
+        profileUserName_Text.text = "";
+        OpenLoginPanel();
+    }
+
+    void CreateUser(string email, string password, string Username)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+
+            UpdateUserProfile(Username);
+        });
+    }
+
+    public void SignInUser(string email, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+
+            profileUserName_Text.text = "" + result.User.DisplayName;
+            profileUserEmail_Text.text = "" + result.User.Email;
+            OpenProfilePanel();
+        });
+    }
+
+    void InitializeFirebase()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+    }
+
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null
+                && auth.CurrentUser.IsValid();
+            if (!signedIn && user != null)
+            {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                Debug.Log("Signed in " + user.UserId);
+                isSignIn = true;
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        auth.StateChanged -= AuthStateChanged;
+        auth = null;
+    }
+
+    void UpdateUserProfile(string UserName)
+    {
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user != null)
+        {
+            Firebase.Auth.UserProfile profile = new Firebase.Auth.UserProfile
+            {
+                DisplayName = UserName,
+                PhotoUrl = new System.Uri("https://placehold.co/150"),
+            };
+            user.UpdateUserProfileAsync(profile).ContinueWith(task =>
+            {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("UpdateUserProfileAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("UpdateUserProfileAsync encountered an error: " + task.Exception);
+                    return;
+                }
+
+                Debug.Log("User profile updated successfully.");
+
+                showNotificationMessage("Alert", "Account Successfully Created");
+            });
+        }
+    }
+
+    bool isSigned = false;
+
+    void Update()
+    {
+        if (isSignIn)
+        {
+            if (!isSigned)
+            {
+                isSigned = true;
+                profileUserName_Text.text = "" + user.DisplayName;
+                profileUserEmail_Text.text = "" + user.Email;
+                OpenProfilePanel();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSE4550_HealthTracker
+A health care app made for our CSE4550 class
